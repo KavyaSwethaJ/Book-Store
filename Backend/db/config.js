a947fb494c5172d14f63fa09567895bb5218adb9
KavyaSwethaJ <j.kavyaswetha@gmail.com>
2024-11-15 17:04:25 +0530

    Add Backend

---
+++ Backend/db/config.js
@@ -0,0 +1,8 @@
+// mongodb://localhost:27017 
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/BookStore1', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+});
